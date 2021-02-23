e1beaf9ae673233f0ff511865996f9b2c144d8a6
Owen <34815371+Stormbreaker187@users.noreply.github.com>
2021-02-22 21:31:25 -0600

    Delete asset directory

--- asset/js/autoplay.js
+++
@@ -1,8 +0,0 @@
-var source = "background.wav"
-var audio = document.createElement("audio");
-audio.autoplay = true;
-audio.load()
-audio.addEventListener("load", function() { 
-    audio.play(); 
-}, true);
-audio.src = source;
